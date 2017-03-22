12b70205b3384b5513f27a9803f3037fe05845fb
max-miksher <max-miksher@mail.ru>
2017-03-22 19:32:18 +0300

    Эксперименты с curl

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 <html>
     <head>
         <title>Статистика</title>
+        <link rel="stylesheet" href="css/style.css"/>
     </head>
     <body>
-        
+        <div class="container">
+            <div class="left_sidbar white">
+            </div>
+            <div class="main">
+                <div class="main__center">
+                    <div class="header black bold">
+                        Общая статистика по сайту
+                    </div>
+                    <div class="content">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
--- write_res.php
+++ write_res.php
@@ -139,3 +139,65 @@ function pars_subcategory()
     };
     /*-------------------------------------------------------------------*/
 }
+
+function get_inf_of_count_item()
+{
+    global $main_url, $list_menu_items,$page_get_request;
+/*--------------------------получаем информацию о страницах для парсинга--------------------*/
+
+    foreach ($list_menu_items as $key => $category) {                           //проходим по всем категориям
+        foreach ($category['subcategories'] as $cat_key => $subcategory) {      //проходим по всем подкатегориям
+            $html_temp = file_get_contents($main_url . $subcategory['link']);   //для каждой подкатегории нужно развернуть страницу и получить из нее данные
+            phpQuery::newDocument($html_temp);                                  //создаем класс для этой страницы
+
+            $list_menu_items[$key]['subcategories'][$cat_key]['count_product'] = pq('.total.many>span:not(.active)')->text();//выдираем количество продуктов подкатегории
+            foreach (pq('.pager-bottom .pager .pageToInsert')->children('a') as $k => $v) {//получаем количество страниц
+                if ($k + 2 == pq('.pager-bottom .pager .pageToInsert')->children('a')->count())
+                    $list_menu_items[$key]['subcategories'][$cat_key]['count_page'] = pq($v)->html();
+            }
+
+            phpQuery::unloadDocuments();//убиваем класс страницы подкатегории
+        }
+    }
+
+/*------------------------------------------------------------------------------------------*/
+}
+
+function pars_page($subcat)
+{
+    /*--------------------------------парсим страницы--------------------------------------------*/
+global $main_url, $list_menu_items,$page_get_request;
+
+foreach ($list_menu_items as $key => $category) {//проходим по всем категориям
+    foreach ($category['subcategories'] as $cat_key => $subcategory) {//проходим по всем подкатегориям
+        for ($i = 1; $i <=$subcategory['count_page']; $i++) 
+        {
+            $html_temp = file_get_contents($main_url . $subcategory['link'].$page_get_request.$i); //для каждой подкатегории нужно развернуть страницу и получить из нее данные
+            phpQuery::newDocument($html_temp);//создаем класс для этой страницы            
+            foreach (pq('.catalog_main_table .ref_goods_n_p') as $q => $qq) //проходим по всем товарам на страницу
+            {
+                $id = pq($qq)->children('.l_class')->attr('id');    //вытаскиваем идентификатор товара
+                $link = pq($qq)->attr('href');                      //сылку на товар
+                  
+                if(pq($qq)->children('.price')->children('ins')->html()=='')   //цену на товар, новую и старую. если есть
+                {
+                    $price_old = preg_replace("/[^0-9]/", '',pq($qq)->children('.price')->text());
+                    $price_new = '';
+                }
+                else
+                {
+                    $price_old = preg_replace("/[^0-9]/", '',pq($qq)->children('.price')->children('ins')->text());
+                    $price_new = preg_replace("/[^0-9]/", '',pq($qq)->children('.price')->children('del')->text());
+                }
+                
+                /*$list_menu_items[$key]['subcategories'][$cat_key]['items'][$q]['id'] = $id;
+                $list_menu_items[$key]['subcategories'][$cat_key]['items'][$q]['link'] = $link;
+                $list_menu_items[$key]['subcategories'][$cat_key]['items'][$q]['price_old'] = $price_old;
+                $list_menu_items[$key]['subcategories'][$cat_key]['items'][$q]['price_new'] = $price_new;*/
+            }
+            phpQuery::unloadDocuments();
+        }
+    }
+}
+/*-------------------------------------------------------------------------------------------*/
+}
